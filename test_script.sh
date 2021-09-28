d85bb78ab89f3082b215b235c7390343c7c33118
Kyle Gerheiser <Kyle.Gerheiser@clogin05.cactus.wcoss2.ncep.noaa.gov>
2021-09-28 21:09:15 +0000

    Add files

--- test_script.sh
+++ test_script.sh
@@ -13,6 +13,4 @@ export FORT31=
 export FORT51=dustconus.t${cyc}z.${type}.1hr_227.grib2.temp
 
 TOCGRIB2SUPER=/apps/ops/prod/libs/intel/19.1.3.304/grib_util/1.2.3/bin/tocgrib2super
-#TOCGRIB2SUPER=/u/Kyle.Gerheiser/dustcs.20210824_test/NCEPLIBS-grib_util/build/install/bin/tocgrib2super
-PARMutil=/lfs/h2/emc/physics/noscrub/Ho-Chun.Huang/wcoss2_transition/util/parm
-${TOCGRIB2SUPER} < $PARMutil/grib2_dustconus_${type}.t${cyc}z.227
+${TOCGRIB2SUPER} < ./grib2_dustconus_${type}.t${cyc}z.227
